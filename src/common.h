065b7d6962922d25654de091bec1e4ae52d704c6
tanneberger <github@tanneberger.me>
2025-03-05 08:55:00 +0100

    RedundantNode works Federation not

--- src/common.h
+++ src/common.h
@@ -1,11 +1,19 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-    typedef enum ForwardInformation ForwardInformation;
+typedef enum ForwardInformation ForwardInformation;
 
-    enum ForwardInformation {
-        ForwardLeft = 1,
-        ForwardRight = 2,
-        ForwardSelf = 3 
-    };
+enum ForwardInformation {
+    ForwardLeft = 1,
+    ForwardRight = 2,
+    ForwardSelf = 3 
+};
+
+typedef struct FederatedData FederatedData;
+
+struct FederatedData {
+    int source;
+    int value;
+};
+
 #endif //COMMON_H
